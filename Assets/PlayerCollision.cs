ab409a380d11f945b0207167a528fd52bc7be6e7
YanZhu00 <82896338+YanZhu00@users.noreply.github.com>
2023-03-29 17:20:20 +0900

    add unity project

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    int flag = 0;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle" && flag == 0)
+        {
+            Debug.Log("We hit an obstacle!");
+            //movement.enabled = false;
+            flag = 1;
+        }
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float forwardForce = 500;
+    public float sideForce = 500;
+    float jumpForce = 6;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb.AddForce(0, 0, forwardForce);
+    }
+    //We marked this as "Fixed"Update because we are using it to mess with physics
+    void FixedUpdate ()
+    {
+        //Debug.Log(Time.deltaTime);
+        //rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("up"))
+        {   
+            if (rb.transform.position.y < 1.01 && rb.velocity.z < 30)
+                rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("left"))
+        {
+            if (rb.transform.position.y < 1.01 && rb.velocity.x > -10)
+                rb.AddForce(-sideForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("right"))
+        {
+            if (rb.transform.position.y < 1.01 && rb.velocity.x < 10)
+                rb.AddForce(sideForce * Time.deltaTime ,0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("down"))
+        {
+            if (rb.transform.position.y < 1.01 && rb.velocity.z > -10)
+                rb.AddForce(0, 0, -forwardForce * Time.deltaTime, ForceMode.VelocityChange);
+        }
+        /*
+        if (Input.GetKey("space"))
+        {
+            Vector3 bar = transform.position;
+            if (bar.y == 1.0)
+                rb.AddForce(0, forwardForce * Time.deltaTime, 0, ForceMode.VelocityChange);
+        }
+        */
+        if (Input.GetKey("space"))
+        {
+            Vector3 bar = transform.position;
+            if (bar.y < 1.01)
+            {
+                float f = 6;
+                rb.AddForce(0, jumpForce, 0, ForceMode.VelocityChange);
+            }
+        }
+    }
+}
